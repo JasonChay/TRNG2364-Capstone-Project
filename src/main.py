ae8a4df2c213d1121c4bb4b0166f8d7e5a2d2d5c
JasonChay <chayjason85@gmail.com>
2026-02-01 19:27:00 -0500

    Created schema.yaml which defines validation constraints and implemented validation.py

--- src/main.py
+++ src/main.py
@@ -17,23 +17,24 @@
 # CURRENTLY, main.py WILL BE USED FOR TESTING EARLY CODE AND DOING SOME EXPLORATORY ANALYSIS ON THE DATA
 
 from ingestion.csv_reader import read_csv
+from validation import validate
 
-# Show some info on our dataset
-chocolate_df = read_csv('.\data\Chocolate Sales.csv')
-print(chocolate_df.info())
-print(chocolate_df.head())
-print(chocolate_df.isnull().sum()) # Print the number of Null values in each column
-# this data is already clean
+# # Show some info on our dataset
+# chocolate_df = read_csv('.\data\Chocolate Sales.csv')
+# print(chocolate_df.info())
+# print(chocolate_df.head())
+# print(chocolate_df.isnull().sum()) # Print the number of Null values in each column
+# # this data is already clean
 
-print('--------------------------------')
+# print('--------------------------------')
 
-# Show some info on another potential dataset
-chocolate2_df = read_csv('./data/chocolate_sales2.csv')
-print(chocolate2_df.info())
-print(chocolate2_df.head())
-print(chocolate2_df.isnull().sum()) 
+# # Show some info on another potential dataset
+# chocolate2_df = read_csv('./data/chocolate_sales2.csv')
+# print(chocolate2_df.info())
+# print(chocolate2_df.head())
+# print(chocolate2_df.isnull().sum()) 
 
-print('--------------------------------')
+# print('--------------------------------')
 
 # Show some info on another potential dataset
 retail_df = read_csv('./data/retail_store_sales.csv')
@@ -41,3 +42,6 @@ print(retail_df.info())
 print(retail_df.head())
 print(retail_df.isnull().sum()) # This data is dirty so we can clean it
 
+accepted, rejected = validate(retail_df)
+print(len(accepted))
+print(len(rejected))
--- src/validation.py
+++ src/validation.py
@@ -1,5 +1,78 @@
 # Validate and check the data AFTER reading from the .csv file and BEFORE cleaning
-# for each row, check if:
+# for each row, we want to determine if the data is usable
 #   - all required fields exist
 #   - all data types are what we expect them to be
 #   - anything else to make sure the data makes sense
+
+import pandas as pd
+import yaml
+
+def validate(df):
+    accepted = []
+    rejected = []
+
+    # We could later add .yaml filepath as an argument to validate function so that it can validate by multiple schemas
+    file = open('./config/schema.yaml', 'r')
+    schema = yaml.safe_load(file)
+
+    required_fields = schema['validation_rules']['required_fields']
+    field_types = schema['validation_rules']['field_types']
+    constraints = schema['validation_rules']['constraints']
+
+    # First checking if data contains all required columns
+    missing_cols = [col for col in required_fields if col not in df.columns]
+    if missing_cols:
+        # If missing required columns, do not continue using this data
+        raise Exception("File is missing required columns:", missing_cols)
+    
+    # Log if there are any extra columns, but allow the data to pass
+    extra_cols = [col for col in df.columns if col not in required_fields]
+    if extra_cols:
+        # Have logger write warning
+        pass
+
+    # Now checking each row
+    for index, row in df.iterrows():
+        errors = [] # put meaningful error messages in here
+
+        # Check that required columns have existing values
+        for required_field in required_fields:
+            if pd.isna(row[required_field]):
+                errors.append(f'Missing or null value in column: {required_field}')
+
+        # Check that all fields match their expected data type
+        for field, required_type in field_types.items():
+            val_in_row = row[field]
+            try:
+                val_in_row = cast(val_in_row, required_type)
+            except Exception:
+                errors.append(f'Value in {field} is not type: {required_type}')
+        
+        # Check all constraints
+        # Add another if statement to add more constraints 
+        for field, constraint_dict in constraints.items():
+            val_in_row = row[field]
+            if 'min' in constraint_dict:
+                if val_in_row < constraint_dict['min']:
+                    errors.append(f'Value in {field} is less than minimum: {constraint_dict["min"]}')
+
+        # Finally accept or reject the row
+        if errors:
+            rejected.append(row)
+            errors = []
+        else:
+            accepted.append(row)
+
+    return accepted, rejected
+
+def cast(var, type):
+    if type == 'str':
+        str(var)
+    elif type == 'int':
+        int(var)
+    elif type == 'float':
+        float(var)
+    elif type == 'bool':
+        bool(var)
+    else:
+        raise ValueError("Unexpected data type passed in")
